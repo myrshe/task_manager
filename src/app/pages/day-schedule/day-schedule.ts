340ae80f0767de9b338197822d2930c22c33a94a
myrshe <polina.zimnyackova@gmail.com>
2026-06-10 02:05:38 +0300

    add routes

---
+++ src/app/pages/day-schedule/day-schedule.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-schedule',
+  imports: [],
+  templateUrl: './day-schedule.html',
+  styleUrl: './day-schedule.scss',
+})
+export class DaySchedule {}
