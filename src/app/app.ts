fff7dce53393c836fdd0c820d31457541430d519
myrshe <polina.zimnyackova@gmail.com>
2026-06-10 02:14:42 +0300

    add header

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,12 @@ import { TuiRoot } from '@taiga-ui/core';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./pages/home/home";
+import { Header } from './shared/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TuiRoot, Home],
+  imports: [RouterOutlet, TuiRoot, Home, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-}
+export class App {}
