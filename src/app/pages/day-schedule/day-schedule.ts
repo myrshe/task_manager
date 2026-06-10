8d4ae4203f38e15d90a6f23195978d5c1670a6df
myrshe <polina.zimnyackova@gmail.com>
2026-06-10 03:28:43 +0300

    add day schedule page

--- src/app/pages/day-schedule/day-schedule.ts
+++ src/app/pages/day-schedule/day-schedule.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { TaskList } from '../../shared/components/task-list/task-list';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-day-schedule',
-  imports: [],
+  imports: [TaskList],
   templateUrl: './day-schedule.html',
   styleUrl: './day-schedule.scss',
 })
-export class DaySchedule {}
+export class DaySchedule {
+  private readonly taskService = inject(TaskService);
+
+  readonly todayTasks = computed(() => {
+    return this.taskService.sortByDeadline(this.taskService.getTodayTasks(), 'asc');
+  });
+
+  readonly weekTopTasks = computed(() => {
+    return this.taskService
+      .sortByPriority(
+        this.taskService.getWeekTasks().filter((task) => task.status !== 'done'),
+        'desc',
+      )
+      .slice(0, 5);
+  });
+
+  readonly monthTopTasks = computed(() => {
+    return this.taskService
+      .sortByPriority(
+        this.taskService.getMonthTasks().filter((task) => task.status !== 'done'),
+        'desc',
+      )
+      .slice(0, 5);
+  });
+}
